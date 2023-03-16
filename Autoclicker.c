9331888fb1c73a13f117e6b412955cb003360fe6
Mixaffected <marvin.dziedzina@gmail.com>
2023-03-16 18:05:49 +0100

    Added comments

--- Autoclicker.c
+++ Autoclicker.c
@@ -3,10 +3,12 @@
 
 int main()
 {
+    // alt key
     wint_t KEYCODE = 0x12;
     int delay;
     int cps;
     printf("How many clicks per seconds do you want?\n");
+    printf("Input 0 to click as fast as possible.\n");
     scanf("%d", &cps);
     if (cps <= 0)
     {
@@ -17,16 +19,21 @@ int main()
         delay = 1000 / cps;
     }
 
+    // clear console
     system("cls");
 
+    // wait for alt key to be pressed
     printf("Click alt\n");
     while (1)
     {
+        // sleep 5 milliseconds to not let your pc burn like the sun
         Sleep(5);
         while (GetAsyncKeyState(KEYCODE))
         {
+            // one click
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            // wait the desired amount so the clicks per second are reached
             Sleep(delay);
         }
     }
